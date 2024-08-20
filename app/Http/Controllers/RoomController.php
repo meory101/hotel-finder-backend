c0943701f4e0be7460a801f70cd554f0fe72fd13
meory101 <othmannour2001@gmail.com>
2024-08-20 14:07:14 +0300

    fixing bugs

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -241,52 +241,8 @@ class RoomController extends Controller
                     array_push($relatedRooms, $rooms[$j]);
                     continue;
                 }
-                // for ($j = 0; $j < count($view); $j++) {
-                //     array_push($vnames, ViewModel::where('id', $view[$j]->view_id)->first());
-                // }
-                // for ($t = 0; $t < count($tools); $t++) {
-                //     array_push($tnames, ToolModel::where('id', $tools[$t]->tool_id)->first());
-                // }
-                // for($h=0 ; $h<count($view) ;$h++){
-                //     print(similar_text($request->tags[$i], $view[$h]->name, $percent) >= 2);
-                //     if (similar_text($request->tags[$i], $view[$h]->name, $percent) >= 2) {
-                //         array_push($relatedRooms, $rooms[$j]);
-                //         continue;
-                //     }       
-                // }
-                // for ($n = 0; $n < count($tools); $n++) {
-                //     if (similar_text($request->tags[$i], $tools[$n]->name, $percent) >= 2) {
-                //         array_push($relatedRooms, $rooms[$j]);
-                //         continue;
-                //     }
-                // }
             }
         }
         return $relatedRooms;
     }
 }
-// $relatedRooms = [];
-// $rooms = RoomModel::all();
-// for ($i = 0; $i < count($request->tags); $i++) {
-//     for ($j = 0; $j < count($rooms); $j++) {
-//         if ($rooms::where($rooms[$j]->name, $request->tags[$i])->first()) {
-//             array_push($relatedRooms, $rooms[$j]);
-//             continue;
-//         }
-//     }
-// }
-
-
-
-// $relatedRooms = [];
-// $rooms = RoomModel::all();
-// for ($i = 0; $i < count($request->tags); $i++) {
-//     for ($j = 0; $j < count($rooms); $j++) {
-//         print($rooms[$j]::where('name', '=', '%' . $request->tags[$i] . '%') || $rooms[$j]::Where('desc', '=', '%' . $request->tags[$i] . '%') || $rooms[$j]::Where('capacity', '=', '%' . $request->tags[$i] . '%'));
-//         if ($rooms[$j]::where('name', '=', '%' . $request->tags[$i] . '%') || $rooms[$j]::Where('desc', '=', '%' . $request->tags[$i] . '%') || $rooms[$j]::Where('capacity', '=', '%' . $request->tags[$i] . '%')) {
-//             // print($rooms[$j]->id);
-//             array_push($relatedRooms, $rooms[$j]);
-//         }
-//     }
-// }
-// return $relatedRooms;
